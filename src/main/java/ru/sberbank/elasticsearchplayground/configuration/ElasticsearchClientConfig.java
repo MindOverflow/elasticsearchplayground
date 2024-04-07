92f84a3f5038873e3dd38e8516997e2f5a4879b8
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2024-04-07 17:31:30 +0300

    Конфигурация с новым spring-data-elasticsearch 5-2-4 без WARN-логов при старте приложения

--- src/main/java/ru/sberbank/elasticsearchplayground/configuration/ElasticsearchClientConfig.java
+++ src/main/java/ru/sberbank/elasticsearchplayground/configuration/ElasticsearchClientConfig.java
@@ -1,27 +1,21 @@
 package ru.sberbank.elasticsearchplayground.configuration;
 
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "ru.sberbank.elasticsearchplayground.repositories")
 @ComponentScan(basePackages = {"ru.sberbank.elasticsearchplayground"})
-public class ElasticsearchClientConfig extends AbstractElasticsearchConfiguration {
+public class ElasticsearchClientConfig extends ElasticsearchConfiguration {
     @Override
-    @Bean
-    public RestHighLevelClient elasticsearchClient() {
-        final ClientConfiguration clientConfiguration = ClientConfiguration
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration
                 .builder()
                 .connectedTo("185.198.152.125:9200")
                 .build();
-        return RestClients.create(clientConfiguration).rest();
     }
 }
