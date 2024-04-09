c23bbe7e7d32de48594d370f742e5dd0e40ff7b1
Коваленко Антон <Kovalenko.A.Andr@sberbank.ru>
2024-04-09 15:30:11 +0300

    Добавили работоспособный сервис ProductSearchService, класс для тестов ProductSearchServiceTest, REST сервис SearchService c REST endpoints: search, suggestions

--- src/main/java/ru/sberbank/elasticsearchplayground/controller/SearchController.java
+++ src/main/java/ru/sberbank/elasticsearchplayground/controller/SearchController.java
@@ -1,13 +1,39 @@
 package ru.sberbank.elasticsearchplayground.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.sberbank.elasticsearchplayground.services.ProductSearchService;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
+@Slf4j
 public class SearchController {
+
+    private final ProductSearchService productSearchService;
+
+    @Autowired
+    public SearchController(ProductSearchService productSearchService) {
+        this.productSearchService = productSearchService;
+    }
 
     @GetMapping("/search")
     public String search() {
         return "search.html";
+    }
+
+    @GetMapping("/suggestions")
+    @ResponseBody
+    public List<String> fetchSuggestions(@RequestParam(value = "q", required = false) String query) {
+        log.info("fetch suggests {}", query);
+        List<String> suggests = productSearchService.fetchSuggestions(query);
+        log.info("suggests {}", suggests);
+        return suggests;
     }
 }
---
+++ src/test/java/ru/sberbank/elasticsearchplayground/services/ProductSearchServiceTest.java
@@ -0,0 +1,85 @@
+package ru.sberbank.elasticsearchplayground.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.sberbank.elasticsearchplayground.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class ProductSearchServiceTest {
+
+    @Autowired
+    private ProductSearchService productSearchService;
+
+    @Test
+    void testCreateProductIndexBulk() {
+        List<Product> products = new ArrayList<Product>();
+        products.add(Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("New Apple iPhone 12 Pro Max (128GB) - Pacific Blue")
+                .category("phone")
+                .price(2300.0)
+                .quantity(55)
+                .manufacturer("apple")
+                .description("6.7-inch Super Retina XDR display\n" +
+                        "Ceramic Shield, tougher than any smartphone glass\n" +
+                        "A14 Bionic chip, the fastest chip ever in a smartphone\n" +
+                        "Pro camera system with 12MP Ultra Wide, Wide and Telephoto cameras; 5x optical zoom range; Night mode, Deep Fusion, Smart HDR 3, Apple ProRAW, 4K Dolby Vision HDR recording")
+                .build()
+        );
+
+        products.add(Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("New Apple iPhone 12 Mini (64GB) - Blue")
+                .category("phone")
+                .price(2300.0)
+                .quantity(15)
+                .manufacturer("apple")
+                .description("5.4-inch Super Retina XDR display\n" +
+                        "Ceramic Shield, tougher than any smartphone glass\n" +
+                        "A14 Bionic chip, the fastest chip ever in a smartphone\n" +
+                        "Advanced dual-camera system with 12MP Ultra Wide and Wide cameras; Night mode, Deep Fusion, Smart HDR 3, 4K Dolby Vision HDR recording\n" +
+                        "12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording\n" +
+                        "Industry-leading IP68 water resistance")
+                .build()
+        );
+
+        products.add(Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("Samsung Galaxy Note10 Lite (Aura Glow, 8GB RAM, 128GB Storage)")
+                .category("phone")
+                .price(2100.0)
+                .quantity(25)
+                .manufacturer("samsung")
+                .description("12MP Ultra wide (123°) FF + F2.2 \" Wide (77°) 12MP AF F1.7 Dual Pixel + Tele (45°) 12MP AF F2.4 OIS, 2x Zoom camera | 32MP front facing camera\n" +
+                        "17.04 centimeters (6.7-inch) super Amoled infinity-O display and FHD+ capacitive multi-touch touchscreen with 2400 x 1080 pixels resolution | 16M color support\n" +
+                        "Memory, Storage & SIM: 8GB RAM | 128GB internal memory expandable up to 1TB | Dual SIM dual-standby (4G+4G)\n" +
+                        "Android v10.0 operating system with 2.7GHz+1.8GHz Exynos 9810 octa core processor")
+                .build()
+        );
+
+        products.add(Product.builder()
+                .id(UUID.randomUUID().toString())
+                .name("Samsung 163 cm (65 Inches) Q Series 4K Ultra HD QLED")
+                .category("television")
+                .price(2100.0)
+                .quantity(5)
+                .manufacturer("samsung")
+                .description("Resolution: 4K Ultra HD (3840x2160) | Refresh Rate: 120 hertz\n" +
+                        "Connectivity: 3 HDMI ports to connect set top box, Blu Ray players, gaming console | 2 USB ports to connect hard drives and other USB devices\n" +
+                        "Sound : 40 Watts Output\n" +
+                        "Display : QLED Panel | Q HDR Elite (HDR 10+) | Slim and stylish design")
+                .build()
+        );
+        List<String> documentIDs = productSearchService.createProductIndexBulk(products );
+        log.info("documentIDs {}", documentIDs);
+    }
+}
